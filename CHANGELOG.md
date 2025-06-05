563907ff19d515b1372ad0b8c254974423aca40b
Lars Wikman <lars@underjord.io>
2025-06-05 16:48:41 +0200

    Move Bun things away from the working directory

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## v0.1.1
 
 Initial release. Includes an igniter installer to provide a default `.cspell.json` config.
+
+## v0.1.2
+
+Make bun not drop JS project garbage everywhere.
--- lib/mix/tasks/spellrider.check.ex
+++ lib/mix/tasks/spellrider.check.ex
@@ -17,13 +17,37 @@ defmodule Mix.Tasks.Spellweaver.Check do
 
     :ok = Bun.install()
 
-    with 0 <- Bun.run(:spellweaver, ~w(add cspell)),
-         0 <- Bun.run(:spellweaver, ~w(run cspell . --quiet)) do
+    working_dir =
+      :code.priv_dir(:spellweaver)
+      |> Path.join("bun")
+
+    File.mkdir_p(working_dir)
+
+    current_dir = File.cwd!()
+
+    cspell_path =
+      working_dir
+      |> Path.join("node_modules/cspell/dist/esm/app.mjs")
+
+    with 0 <- add(~w(cspell --cwd=#{working_dir})),
+         0 <- run_(~w(#{cspell_path} #{current_dir} --quiet --cwd=#{working_dir})) do
       Mix.shell().info("Spellcheck passed.")
     else
       status ->
         Mix.shell().error("Spellcheck failed.")
         System.halt(status)
     end
+  end
+
+  defp add(args) do
+    bun("add", args)
+  end
+
+  defp run_(args) do
+    bun("run", args)
+  end
+
+  defp bun(command, args) do
+    Bun.run(:spellweaver, [command | args])
   end
 end
