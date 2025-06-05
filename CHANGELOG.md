72ec1696e9ac398c3aa553f23edc066fbecadd93
Lars Wikman <lars@underjord.io>
2025-06-05 16:52:59 +0200

    Tag v0.1.3 because I'm goofy

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,6 @@
 
 Initial release. Includes an igniter installer to provide a default `.cspell.json` config.
 
-## v0.1.2
+## v0.1.3
 
 Make bun not drop JS project garbage everywhere.
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Spellweaver.MixProject do
   def project do
     [
       app: :spellweaver,
-      version: "0.1.0",
+      version: "0.1.3",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       consolidate_protocols: Mix.env() != :dev,
